47def70f43642fadf591bc4b75f38125dd933d19
franco <mansilla.franco.1@gmail.com>
2023-08-02 14:39:44 -0300

    config back

---
+++ api/src/models/User.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define('user', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    }
+  }, { timestamps: false })
+}
