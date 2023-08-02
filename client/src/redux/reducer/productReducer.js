1b4eb4e54c83ad64da5c4e3d33a99cafc3b7b335
franco <mansilla.franco.1@gmail.com>
2023-08-02 10:53:38 -0300

    config front

---
+++ client/src/redux/reducer/productReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  // products: [],
+  // allProducts: [],
+};
+
+function productReducer(state = initialState, action) {
+  switch(action.type) {
+  }
+}
+
+
+export default productReducer;
