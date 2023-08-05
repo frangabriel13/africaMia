8843458fefd81cb4a7569edc18fe6383e799a524
franco <mansilla.franco.1@gmail.com>
2023-08-05 18:22:45 -0300

    ruta creada

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const categoryRoute = require('./categoryRoute');
+const productRoute = require('./productRoute');
 
 const router = Router();
 
 router.use('/', categoryRoute);
+router.use('/', productRoute);
 
 
 module.exports = router;
