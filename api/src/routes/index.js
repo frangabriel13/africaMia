d3339ec12771b6492631656535ebbb8a4887d88e
franco <mansilla.franco.1@gmail.com>
2023-08-08 18:45:37 -0300

    img cambio

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const categoryRoute = require('./categoryRoute');
 const productRoute = require('./productRoute');
+const imageRoute = require('./imageRoute');
 
 const router = Router();
 
 router.use('/', categoryRoute);
 router.use('/', productRoute);
+router.use('/', imageRoute);
 
 
 module.exports = router;
