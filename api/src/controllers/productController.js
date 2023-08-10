ea6b0fd1b78ff2120333e845ae5b4892494ea8cc
franco <mansilla.franco.1@gmail.com>
2023-08-10 11:06:20 -0300

    nuevos modelos

--- api/src/controllers/productController.js
+++ api/src/controllers/productController.js
@@ -1,47 +1,6 @@
 const { Product, Category, Variation } = require('../db');
 const { Op } = require("sequelize");
 
-const getProduct = async (req, res) => {
-  const { name } = req.query;
-  let products;
-
-  if(name) {
-    products = await Product.findAll({
-      where: {
-        [op.or]: [
-          { name: { [Op.iLike]: `%${name}%` } },
-          { name: { [Op.iLike]: `%${name.split(" ").join("%")}%` } }
-        ]
-      },
-      include: [
-        {
-          model: Variation,
-          as: 'variations'
-        },
-        {
-          model: Category,
-          as: 'category'
-        }
-      ]
-    })
-  } else {
-    products = await Product.findAll({
-      include: [
-        {
-          model: Variation,
-          as: 'variations'
-        },
-        {
-          model: Category,
-          as: 'category'
-        }
-      ]
-    });
-
-    return products;
-  }
-}
-
 
 module.exports = {
   getProduct,
