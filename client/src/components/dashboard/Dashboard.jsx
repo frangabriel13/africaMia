62a2a6601c4fba6f3a69ad01fbf4422ac724bbbc
franco <mansilla.franco.1@gmail.com>
2023-08-21 18:24:13 -0300

    configure rutas sideBar

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./Dashboard.module.css";
+import { Routes, Route, useLocation } from "react-router-dom";
 import SideBar from "./sideBar/SideBar";
 import Gallery from "./gallery/Gallery";
 
@@ -10,7 +11,15 @@ function Dashboard() {
         <SideBar />
       </div>
       <div className={s.content}>
-        <Gallery />
+        <Routes>
+          <Route path="/" element={<Gallery />} />
+          <Route path="/products" element={<Gallery />} />
+          <Route path="/users" element={<Gallery />} />
+          <Route path="/categories" element={<Gallery />} />
+          <Route path="/variations" element={<Gallery />} />
+          <Route path="/promociones" element={<Gallery />} />
+          <Route path="/otros" element={<Gallery />} />
+        </Routes>
       </div>
     </div>
   );
