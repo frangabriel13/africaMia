7b8c0f30312d44f23099ab05f9be55fe7a157c82
franco <mansilla.franco.1@gmail.com>
2023-08-23 14:26:49 -0300

    css config

--- client/src/components/dashboard/configuration/Configuration.jsx
+++ client/src/components/dashboard/configuration/Configuration.jsx
@@ -6,8 +6,28 @@ function Configuration() {
   return (
     <div className={s.configuration}>
       <h2>Configuración</h2>
-      <div>
-        <Categories />
+      <div className={s.divContainer}>
+        <div className={s.content}>
+          <Categories />
+        </div>
+        <div className={s.divForm}>
+          <h3>Formulario</h3>
+          <form>
+            <label>
+              Nombre:
+              <input type="text" name="name" />
+            </label>
+            <label>
+              Contraseña:
+              <input type="password" name="password" />
+            </label>
+            <label>
+              Foto de perfil:
+              <input type="file" name="profilePicture" />
+            </label>
+            <input type="submit" value="Enviar" />
+          </form>
+        </div>
       </div>
     </div>
   );
