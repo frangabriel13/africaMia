203805b321ee60e62c8ece43e5ab54c9b9476957
franco <mansilla.franco.1@gmail.com>
2023-09-06 13:56:37 -0300

    formulario creado

---
+++ client/src/components/dashboard/productManagement/Images.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import s from "./Images.module.css";
+import { useDispatch, useSelector } from "react-redux";
+
+function Images() {
+  return(
+    <div>
+      <h1>Images</h1>
+    </div>
+  )
+}
+
+
+export default Images;
---
+++ client/src/components/dashboard/productManagement/ProductForm.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from "react";
+import s from "./ProductForm.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addProduct } from "../../../redux/actions/productActions";
+import { getCategories } from "../../../redux/actions/categoryActions";
+import { getImages } from "../../../redux/actions/imageActions";
+import Images from "./Images";
+
+function ProductForm() {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.category.categories);
+  const images = useSelector((state) => state.gallery.images);
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    image: {},
+    price: 0,
+    stock: 0,
+    categoryId: "",
+    imgMain: "",
+    isVariable: false,
+    availability: true,
+  });
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
+
+  useEffect(() => {
+    dispatch(getImages());
+  }, []);
+
+  return (
+    <div className={s.container}>
+      <h2>Crear Producto</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Nombre:</label>
+          <input 
+            type="text" 
+            name="name" 
+            value={formData.name} 
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })} 
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Descripción:</label>
+          <textarea 
+            name="description" 
+            value={formData.description} 
+            onChange={(e) => setFormData({ ...formData, description: e.target.value })} 
+          />
+        </div>
+        <div>
+          <label htmlFor="price">Precio:</label>
+          <input 
+            type="number" 
+            name="price" 
+            value={formData.price} 
+            onChange={(e) => setFormData({ ...formData, price: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="stock">Stock:</label>
+          <input 
+            type="number" 
+            name="stock" 
+            value={formData.stock} 
+            onChange={(e) => setFormData({ ...formData, stock: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="category">Categoría:</label>
+          <select 
+            name="category" 
+            value={formData.categoryId} 
+            onChange={(e) => setFormData({ ...formData, categoryId: e.target.value })}
+          >
+            <option value="">Seleccionar</option>
+            {
+              categories.map((el) => (
+                <option key={el.id} value={el.id}>{el.name}</option>
+              ))
+            }
+          </select>
+        </div>
+        <div>
+          <label htmlFor="image">Imágenes:</label>
+          <Images />
+        </div>
+        <div>
+          <button>Añadir</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+
+export default ProductForm;
