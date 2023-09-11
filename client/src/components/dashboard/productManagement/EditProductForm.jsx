1abbdffdc9ecac5b7f282f6531bee618a54e364d
franco <mansilla.franco.1@gmail.com>
2023-09-11 14:45:51 -0300

    edit configurado

---
+++ client/src/components/dashboard/productManagement/EditProductForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './EditProductForm.module.css';
+
+function EditProductForm() {
+  return(
+    <div className={s.container}>
+      <h3>Formulario de edición de producto</h3>
+    </div>
+  )
+}
+
+
+export default EditProductForm;
