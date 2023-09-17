adfe47c4c06d4bb4ff86b4fa79a0d529639b6914
fabio mansilla <fabio.mansilla07@gmail.com>
2023-09-17 19:45:54 -0300

    lorenzoide

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,32 +1,56 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCategories } from '../../redux/actions/categoryActions';
 import s from './Categories.module.css';
-import { Link } from 'react-router-dom'; // Importa Link de React Router
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
+import { Link } from 'react-router-dom';
 
 function Categories() {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.category.categories);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const navigate = useNavigate(); // Inicializa useNavigate
 
   useEffect(() => {
     // Cuando el componente se monta, obtén todas las categorías
     dispatch(getCategories());
   }, [dispatch]);
 
+  const handleCategoryChange = (event) => {
+    const categoryId = event.target.value;
+    setSelectedCategory(categoryId);
+
+    if (categoryId) {
+      // Redirige a la página de la categoría seleccionada
+      navigate(`/categories/${categoryId}`);
+    } else {
+      // Si selecciona "Todas las categorías", redirige a la página principal de categorías
+      navigate('/categories');
+    }
+  };
+
   return (
     <div className={s.categoriesContainer}>
       <h2 className={s.categoryTitle}>Categorías</h2>
-      <ul className={s.categoryList}>
+      <select
+        id="categorySelect"
+        className={s.categorySelector}
+        value={selectedCategory}
+        onChange={handleCategoryChange}
+      >
+        <option value="">Todas las categorías</option>
         {categories.map((category) => (
-          <li key={category.id} className={s.categoryItem}>
-            <Link to={`/categories/${category.id}`}>{category.name}</Link> {/* Enlace a la página de productos */}
-          </li>
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
         ))}
-      </ul>
+      </select>
     </div>
   );
 }
 
 export default Categories;
+
+
 
 
