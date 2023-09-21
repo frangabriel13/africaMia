3b8b4ddaa4d47e460785871807c5b57919ee92ea
franco <mansilla.franco.1@gmail.com>
2023-09-21 18:07:07 -0300

    cart redux config

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -1,9 +1,34 @@
-export const addToCart = (item) => {
+export const addToCart = (product, selectedVariation, quantity) => {
   return {
     type: 'ADD_TO_CART',
-    payload: item,
+    payload: {
+      product,
+      selectedVariation,
+      quantity,
+    },
   };
-}
+  // if (product.isVariable) {
+  //   // Producto variable: Puedes mantener selectedVariation como está
+  //   return {
+  //     type: 'ADD_TO_CART',
+  //     payload: {
+  //       product,
+  //       selectedVariation,
+  //       quantity,
+  //     },
+  //   };
+  // } else {
+  //   // Producto simple: selectedVariation debe ser null, y la cantidad debe ser quantity
+  //   return {
+  //     type: 'ADD_TO_CART',
+  //     payload: {
+  //       product,
+  //       selectedVariation: null,
+  //       quantity,
+  //     },
+  //   };
+  // }
+};
 
 export const removeFromCart = (productId) => {
   return {
--- client/src/redux/reducer/cartReducer.js
+++ client/src/redux/reducer/cartReducer.js
@@ -1,14 +1,25 @@
 const initialState = {
-  cart: [],
+  cartItems: [],
   total: 0,
 };
 
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
+      const { product, selectedVariation, quantity } = action.payload;
+      // console.log(product)
+      // console.log(selectedVariation)
+      // console.log(quantity)
+      const item = {
+        product,
+        selectedVariation,
+        quantity,
+        // selectedVariation: selectedVariation !== undefined ? selectedVariation : null,
+        // quantity: quantity !== undefined ? quantity : 1,
+      };
       return {
         ...state,
-        cart: [...state.cart, action.payload],
+        cartItems: [...state.cartItems, item], // Agrega el nuevo elemento al carrito
       };
     default:
       return state;
