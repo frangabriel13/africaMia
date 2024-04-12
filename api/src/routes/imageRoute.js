8f690f4be7be96267b4535ee0139967e608679cb
Franco <mansilla.franco.1@gmail.com>
2024-04-12 10:14:13 -0300

    nueva rama

--- api/src/routes/imageRoute.js
+++ api/src/routes/imageRoute.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const { Image, Color } = require('../db');
 const cloudinary = require('../utils/cloudinary');
-const upload = require('../utils/multer');
+// const upload = require('../utils/multer');
 const fs = require('fs');
+const { uploadFiles, deleteImage } = require('../middlewares/uploadFiles');
 
 const router = Router();
 
@@ -44,27 +45,18 @@ router.get('/images/:id', async (req, res) => {
   }
 });
 
-router.post('/images', upload.array('images'), async (req, res) => {
-  console.log(req.files)
-  const imagePaths = req.files.map((file) => file.path);
-  // const { colorId } = req.body;
-
+router.post('/images', async (req, res) => {
   try {
-    const uploadedImages = await Promise.all(imagePaths.map((path) => cloudinary.uploader.upload(path)));
-    const imageUrls = uploadedImages.map((image) => image.secure_url);
+    await uploadFiles(req, res);
+
+    const imageUrls = req.files.map((file) => file.location);
 
     const createdImages = await Promise.all(imageUrls.map(async (imageUrl) => {
-      const image = await Image.create({
-        url: imageUrl,
-        // colorId,
-      });
+      const image = Image.create({
+        url:imageUrl,
+      })
       return image;
-    }));
-
-    // Eliminar los archivos temporales
-    imagePaths.forEach((path) => {
-      fs.unlinkSync(path);
-    });
+    }))
 
     res.status(200).json(createdImages);
   } catch(error) {
@@ -90,23 +82,46 @@ router.put('/images/:id', async (req, res) => {
   }
 });
 
+// router.delete('/images/:id', async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const image = await Image.findByPk(id);
+//     if(!image) {
+//       return res.status(404).json({ message: 'Imagen no encontrada' });
+//     }
+
+//     // Eliminar la imagen de cloudinary
+//     const publicId = image.url.split('/').slice(-1)[0].split('.')[0];
+//     await cloudinary.uploader.destroy(publicId);
+
+//     await image.destroy();
+//     res.status(200).json({ message: 'Imagen eliminada correctamente' });
+//   } catch(error) {
+//     res.status(500).json({ message: 'Error al eliminar la imagen' });
+//   } 
+// });
 router.delete('/images/:id', async (req, res) => {
   const { id } = req.params;
+
   try {
-    const image = await Image.findByPk(id);
-    if(!image) {
-      return res.status(404).json({ message: 'Imagen no encontrada' });
-    }
+      // Encuentra la imagen en la base de datos
+      const image = await Image.findByPk(id);
+      if (!image) {
+          return res.status(404).json({ message: 'Imagen no encontrada' });
+      }
 
-    // Eliminar la imagen de cloudinary
-    const publicId = image.url.split('/').slice(-1)[0].split('.')[0];
-    await cloudinary.uploader.destroy(publicId);
+      // Elimina la imagen de Amazon S3 utilizando la función deleteImage
+      await deleteImage(image.url);
 
-    await image.destroy();
-    res.status(200).json({ message: 'Imagen eliminada correctamente' });
-  } catch(error) {
-    res.status(500).json({ message: 'Error al eliminar la imagen' });
-  } 
+      // Elimina la imagen de la base de datos
+      await image.destroy();
+
+      // Responde con un mensaje de éxito
+      res.status(200).json({ message: 'Imagen eliminada correctamente' });
+  } catch (error) {
+      console.error('Error al eliminar la imagen:', error);
+      res.status(500).json({ message: 'Error al eliminar la imagen' });
+  }
 });
 
 
