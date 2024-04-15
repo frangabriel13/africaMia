4c195b7267783d9ad3fef478b1b42a4127aca953
fabio mansilla <fabio.mansilla07@gmail.com>
2024-04-15 09:05:47 -0300

    agregue rezise

--- client/src/components/dashboard/gallery/imageForm/ImageForm.jsx
+++ client/src/components/dashboard/gallery/imageForm/ImageForm.jsx
@@ -1,18 +1,76 @@
+// import React, { useState } from 'react';
+// import s from './ImageForm.module.css';
+// import { useDispatch } from 'react-redux';
+// import { createImage, getImages } from '../../../../redux/actions/imageActions';
+
+// function ImageForm({ setShowForm }) {
+//   const [selectedImages, setSelectedImages] = useState([]);
+
+//   const dispatch = useDispatch(); 
+  
+//   const handleImages = (e) => {
+//     const files = e.target.files;
+//     let images = [];
+//     for (let i = 0; i < files.length; i++) {
+//       images.push(files[i]);
+//     }
+//     setSelectedImages(images);
+//   };
+
+//   const handleSubmitImg = (e) => {
+//     e.preventDefault();
+
+//     const formData = new FormData();
+//     for (let i = 0; i < selectedImages.length; i++) {
+//       formData.append('images', selectedImages[i]);
+//     }
+
+//     dispatch(createImage(formData))
+//       .then(() => {
+//         dispatch(getImages()); // Obtiene las imágenes actualizadas
+//         setShowForm(false);
+//       });
+//   };
+
+//   return (
+//     <div className={s.container}>
+//       <h3 className={s.title}>Agregar imagen</h3>
+//       <form className={s.form} onSubmit={(e) => { e.preventDefault(); handleSubmitImg(e); }} >
+//         <input 
+//           className={s.input} 
+//           type="file" 
+//           multiple 
+//           accept='image/*'
+//           onChange={handleImages}
+//         />
+//         <button type='button' className={s.btn} onClick={() => setShowForm(false)} >Cancelar</button>
+//         <button type='button' className={s.btn} onClick={handleSubmitImg}>Subir</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+
+// export default ImageForm;
+
 import React, { useState } from 'react';
 import s from './ImageForm.module.css';
 import { useDispatch } from 'react-redux';
 import { createImage, getImages } from '../../../../redux/actions/imageActions';
+import { resizeImage } from '../../../../utils/helpers'; // Importa la función resizeImage
 
 function ImageForm({ setShowForm }) {
   const [selectedImages, setSelectedImages] = useState([]);
 
   const dispatch = useDispatch(); 
   
-  const handleImages = (e) => {
+  const handleImages = async (e) => { // Cambia a async para poder usar await
     const files = e.target.files;
     let images = [];
     for (let i = 0; i < files.length; i++) {
-      images.push(files[i]);
+      // Llama a la función resizeImage para cada imagen seleccionada
+      const resizedImage = await resizeImage(files[i]);
+      images.push(resizedImage);
     }
     setSelectedImages(images);
   };
@@ -49,6 +107,5 @@ function ImageForm({ setShowForm }) {
     </div>
   );
 }
-
 
 export default ImageForm;
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -26,3 +26,45 @@ export const randomPhoneNumber = () => {
   const randomNumber = Math.floor(Math.random() * phoneNumbers.length);
   return phoneNumbers[randomNumber];
 };
+
+export const resizeImage = (file) => {
+  return new Promise((resolve) => {
+    const maxSize = 1200; // Puedes ajustar el tamaño máximo según tus necesidades
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = (event) => {
+      const img = new Image();
+      img.src = event.target.result;
+
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        let width = img.width;
+        let height = img.height;
+
+        if (width > height) {
+          if (width > maxSize) {
+            height *= maxSize / width;
+            width = maxSize;
+          }
+        } else {
+          if (height > maxSize) {
+            width *= maxSize / height;
+            height = maxSize;
+          }
+        }
+
+        canvas.width = width;
+        canvas.height = height;
+
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, width, height);
+
+        canvas.toBlob((blob) => {
+          const resizedFile = new File([blob], file.name, { type: file.type });
+          resolve(resizedFile);
+        }, file.type);
+      };
+    };
+  });
+};
