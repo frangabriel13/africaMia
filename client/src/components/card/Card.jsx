bd7e0e0f89edb306356de3612ebac3f0bfe16d56
Franco <mansilla.franco.1@gmail.com>
2024-08-15 17:15:48 -0300

    change

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import s from "./Card.module.css";
 
 const Card = ({ name, price, id, images, imgMain, productId }) => {
+  const [currentImage, setCurrentImage] = useState(imgMain);
+
   const handleCardClick = () => {
     const product = { name, price, id, images };
     handleScrollToTop();
@@ -12,17 +14,28 @@ const Card = ({ name, price, id, images, imgMain, productId }) => {
     console.log('Productito seleccionado');
     window.scrollTo({
       top: 0,
-      behavior: 'smooth', // Desplazamiento suave
+      behavior: 'smooth',
     });
   };
 
-  console.log('images', images);
+  const handleMouseEnter = () => {
+    if(images && images.length > 0) {
+      const newImage = images.find(images => images.url !== imgMain);
+      if(newImage) {
+        setCurrentImage(newImage.url);
+      }
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setCurrentImage(imgMain);
+  };
 
   return (
     <Link to={`/products/${productId}`}>
       <div onClick={handleCardClick} className={s.container}>
-        <div className={s.divImage}>
-          <img src={imgMain} alt={name} />
+        <div className={s.divImage} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+          <img className={`${s.image}`} src={currentImage} alt={name} />
         </div>
         <div className={s.divContent}>
           <h3>{name}</h3>
