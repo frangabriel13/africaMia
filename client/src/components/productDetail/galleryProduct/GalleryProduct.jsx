ebbe4a4643fe387f1dbff38aa5a7c9925b3bfabc
Franco <mansilla.franco.1@gmail.com>
2024-08-16 13:15:06 -0300

    cambios

--- client/src/components/productDetail/galleryProduct/GalleryProduct.jsx
+++ client/src/components/productDetail/galleryProduct/GalleryProduct.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import s from "./GalleryProduct.module.css";
 
 const GalleryProduct = ({ images }) => {
   const [selectedImage, setSelectedImage] = useState(images[0].url);
+  const imageRefs = useRef([]);
 
-  const handleClick = (url) => {
+  const handleClick = (url, index) => {
     setSelectedImage(url);
+    imageRefs.current[index].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
   };
   
   return (
@@ -17,7 +19,7 @@ const GalleryProduct = ({ images }) => {
               key={image.id}
               src={image.url}
               alt={`Thumbnail ${index}`}
-              onClick={() => handleClick(image.url)}
+              onClick={() => handleClick(image.url, index)}
             />
           ))
         }
@@ -30,6 +32,7 @@ const GalleryProduct = ({ images }) => {
               src={image.url}
               alt={`Image ${index}`}
               className={s.image}
+              ref={el => imageRefs.current[index] = el}
             />
           ))
         }
@@ -40,3 +43,48 @@ const GalleryProduct = ({ images }) => {
 
 
 export default GalleryProduct;
+
+// import React, { useState, useRef } from "react";
+// import s from "./GalleryProduct.module.css";
+
+// const GalleryProduct = ({ images }) => {
+//   const [selectedImage, setSelectedImage] = useState(images[0].url);
+//   const imageRefs = useRef([]);
+
+//   const handleClick = (url, index) => {
+//     setSelectedImage(url);
+//     imageRefs.current[index].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+//   };
+
+//   return (
+//     <div className={s.container}>
+//       <div className={s.imageList}>
+//         {
+//           images.map((image, index) => (
+//             <img
+//               key={image.id}
+//               src={image.url}
+//               alt={`Thumbnail ${index}`}
+//               onClick={() => handleClick(image.url, index)}
+//             />
+//           ))
+//         }
+//       </div>
+//       <div className={s.images}>
+//         {
+//           images.map((image, index) => (
+//             <img
+//               key={image.id}
+//               src={image.url}
+//               alt={`Image ${index}`}
+//               className={s.image}
+//               ref={el => imageRefs.current[index] = el}
+//             />
+//           ))
+//         }
+//       </div>
+//     </div>
+//   )
+// };
+
+// export default GalleryProduct;
